0a0474949813ab6cd5e79e820254318bc63fd61d
Steffen Glückselig <gist@gungfu.de>
2021-02-21 10:22:40 +0100

    Pipeline works now.

---
+++ 000_env.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set stack=babashka-lambda-layer
+set s3_bucket=babashka-lambda-layer
+set output_templatefile=temp_template.yaml
+set layer-name=babashka-runtime
--- 003_make_deploy.bat
+++ 003_make_deploy.bat
@@ -1,28 +1,9 @@
-stack = babashka-lambda-layer
-s3-bucket = my-bucket
+@echo off
 
-build:
-	docker build --target BUILDER -t babashka-lambda-archiver .
-	docker rm build || true
-	docker create --name build babashka-lambda-archiver
-	docker cp build:/var/task/babashka-runtime.zip babashka-runtime.zip
+call 000_env.bat
 
-package: build
-	aws cloudformation package \
-        --template-file template.yaml \
-        --s3-bucket $(s3-bucket) \
-        --s3-prefix babashka \
-        --output-template-file /tmp/template.yaml
 
-deploy: package
-	aws cloudformation deploy \
-        --template-file /tmp/template.yaml \
-        --stack-name $(stack) \
-        --capabilities CAPABILITY_IAM \
-        --no-fail-on-empty-changeset
+aws cloudformation deploy --template-file %output_templatefile% --stack-name %stack% --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset
+echo.
+echo.
 
-publish-layer: build
-	aws lambda publish-layer-version \
-		--layer-name babashka-runtime \
-		--principal "*" \
-		--zip-file fileb://babashka-runtime.zip
