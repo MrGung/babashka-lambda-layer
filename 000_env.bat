61bfd7cb46508b8853e676a04654c59ec2bc24ee
Steffen Glückselig <gist@gungfu.de>
2021-02-21 17:05:38 +0100

    Pipeline: automated update of layer in function: 005_update_layer_in_function.bat

--- 000_env.bat
+++ 000_env.bat
@@ -1,6 +1,8 @@
 @echo off
 
+set function_name=babashka-lambda-example
 set stack=babashka-lambda-layer
 set s3_bucket=babashka-lambda-layer
 set output_templatefile=temp_template.yaml
+set output_publishfile=publish.out
 set layer-name=babashka-runtime
