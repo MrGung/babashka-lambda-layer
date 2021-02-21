dc9277bd711397c3b0a41321b3454f0b17b240e1
Steffen Glückselig <gist@gungfu.de>
2021-02-21 17:32:09 +0100

    add-layer-version-permission: principal *

--- 000_env.bat
+++ 000_env.bat
@@ -3,6 +3,7 @@
 set function_name=babashka-lambda-example
 set stack=babashka-lambda-layer
 set s3_bucket=babashka-lambda-layer
+set aws_region=eu-central-1
 set output_templatefile=temp_template.yaml
 set output_publishfile=publish.out
 set layer-name=babashka-runtime
--- 004_make_publish.bat
+++ 004_make_publish.bat
@@ -2,6 +2,14 @@
 
 call 000_env.bat
 
+
 aws lambda publish-layer-version --layer-name %layer-name% --zip-file fileb://babashka-runtime.zip > %output_publishfile%
+
+
 echo.
+
+bb_jq < %output_publishfile% | bb -o "(:Version *input*)" > layer_version.out
+set /P layer_version=<layer_version.out
+
+aws lambda add-layer-version-permission --layer-name %layer-name% --principal "*" --action lambda:GetLayerVersion --version-number %layer_version% --statement-id public --region %aws_region%
 echo.
